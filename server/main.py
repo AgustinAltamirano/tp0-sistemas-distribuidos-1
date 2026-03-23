880a846f758784c3aba2dc5919adbd25a84102bd
AgustinAltamirano <agustinga110704@gmail.com>
2026-03-23 12:21:09 -0300

    feat: handle sigterm signal in client and server

--- server/main.py
+++ server/main.py
@@ -4,16 +4,17 @@ from configparser import ConfigParser
 from common.server import Server
 import logging
 import os
+import signal
 
 
 def initialize_config():
-    """ Parse env variables or config file to find program config params
+    """Parse env variables or config file to find program config params
 
     Function that search and parse program configuration parameters in the
-    program environment variables first and the in a config file. 
-    If at least one of the config parameters is not found a KeyError exception 
-    is thrown. If a parameter could not be parsed, a ValueError is thrown. 
-    If parsing succeeded, the function returns a ConfigParser object 
+    program environment variables first and the in a config file.
+    If at least one of the config parameters is not found a KeyError exception
+    is thrown. If a parameter could not be parsed, a ValueError is thrown.
+    If parsing succeeded, the function returns a ConfigParser object
     with config parameters
     """
 
@@ -23,15 +24,30 @@ def initialize_config():
 
     config_params = {}
     try:
-        config_params["port"] = int(os.getenv('SERVER_PORT', config["DEFAULT"]["SERVER_PORT"]))
-        config_params["listen_backlog"] = int(os.getenv('SERVER_LISTEN_BACKLOG', config["DEFAULT"]["SERVER_LISTEN_BACKLOG"]))
-        config_params["logging_level"] = os.getenv('LOGGING_LEVEL', config["DEFAULT"]["LOGGING_LEVEL"])
+        config_params["port"] = int(
+            os.getenv("SERVER_PORT", config["DEFAULT"]["SERVER_PORT"])
+        )
+        config_params["listen_backlog"] = int(
+            os.getenv(
+                "SERVER_LISTEN_BACKLOG", config["DEFAULT"]["SERVER_LISTEN_BACKLOG"]
+            )
+        )
+        config_params["logging_level"] = os.getenv(
+            "LOGGING_LEVEL", config["DEFAULT"]["LOGGING_LEVEL"]
+        )
     except KeyError as e:
         raise KeyError("Key was not found. Error: {} .Aborting server".format(e))
     except ValueError as e:
-        raise ValueError("Key could not be parsed. Error: {}. Aborting server".format(e))
+        raise ValueError(
+            "Key could not be parsed. Error: {}. Aborting server".format(e)
+        )
 
     return config_params
+
+
+def _handle_term(server):
+    logging.info(f"action: sigterm_received | result: success")
+    server.request_shutdown()
 
 
 def main():
@@ -44,12 +60,16 @@ def main():
 
     # Log config parameters at the beginning of the program to verify the configuration
     # of the component
-    logging.debug(f"action: config | result: success | port: {port} | "
-                  f"listen_backlog: {listen_backlog} | logging_level: {logging_level}")
+    logging.debug(
+        f"action: config | result: success | port: {port} | "
+        f"listen_backlog: {listen_backlog} | logging_level: {logging_level}"
+    )
 
     # Initialize server and start server loop
-    server = Server(port, listen_backlog)
-    server.run()
+    with Server(port, listen_backlog) as server:
+        signal.signal(signal.SIGTERM, lambda _signum, _frame: _handle_term(server))
+        server.run()
+
 
 def initialize_log(logging_level):
     """
@@ -59,9 +79,9 @@ def initialize_log(logging_level):
     compose logs the date when the log has arrived
     """
     logging.basicConfig(
-        format='%(asctime)s %(levelname)-8s %(message)s',
+        format="%(asctime)s %(levelname)-8s %(message)s",
         level=logging_level,
-        datefmt='%Y-%m-%d %H:%M:%S',
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
 
